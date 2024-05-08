82c8182a4d736284ef45eb1d5e004edd322aec78
Aaronphilip2003 <aaronphilip2003@gmail.com>
2024-05-08 20:14:18 +0530

    added adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+// _app.js
+
+import React from 'react';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-  }
+  return (
+    <>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2976969141373216"
+     crossorigin="anonymous"></script>
+      <Component {...pageProps} />
+    </>
+  );
+}
