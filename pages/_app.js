3e25c7185d9dc4377f7a108d70f2f20f7f57deb2
Aaronphilip2003 <aaronphilip2003@gmail.com>
2024-07-01 10:35:56 +0530

    repaired _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2976969141373216"
-     crossorigin="anonymous"></script>
+     crossOrigin="anonymous"></script>
       <Component {...pageProps} />
     </>
   );
